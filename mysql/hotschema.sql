1fddd458e867051be240b7d52dc1077cd4a0613e
Baggio Shehadi <k3nh1n@yahoo.com>
2017-04-21 10:39:54 -0500

    Changed sql file

--- mysql/hotschema.sql
+++ mysql/hotschema.sql
@@ -8,3 +8,15 @@ CREATE TABLE waitlist(
     );
     INSERT INTO hot.waitlist(customerName, phoneNumber, customerEmail, customerID)
     VALUES("John Smith","123-456-7890","testing@test.com","test123");
+    CREATE TABLE topFive(
+	customerName VARCHAR(100) NOT NULL,
+    phoneNumber VARCHAR(100) NOT NULL,
+    customerEmail VARCHAR(100) NOT NULL,
+    customerID VARCHAR(100) NOT NULL
+    );
+        CREATE TABLE mastertable(
+	customerName VARCHAR(100) NOT NULL,
+    phoneNumber VARCHAR(100) NOT NULL,
+    customerEmail VARCHAR(100) NOT NULL,
+    customerID VARCHAR(100) NOT NULL
+    );
