86bf62a94bfe9bbe8915ada9da046b82d3464e10
Adam Brunn <adambrunn1@hotmail.com>
2017-04-21 09:55:08 -0500

    added sql database schema file

---
+++ mysql/hotschema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE hot;
+USE hot;
+CREATE TABLE waitlist(
+	customerName VARCHAR(100) NOT NULL,
+    phoneNumber VARCHAR(100) NOT NULL,
+    customerEmail VARCHAR(100) NOT NULL,
+    customerID VARCHAR(100) NOT NULL
+    );
+    INSERT INTO hot.waitlist(customerName, phoneNumber, customerEmail, customerID)
+    VALUES("John Smith","123-456-7890","testing@test.com","test123");
